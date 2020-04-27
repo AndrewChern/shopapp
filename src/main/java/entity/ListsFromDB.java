3f53f9e66951e395d3c88309ab8f436853cb22f8
Андрей <nrnk3373@gmail.com>
2020-04-27 17:32:03 +0300

    Lists from DB added

---
+++ src/main/java/entity/ListsFromDB.java
@@ -0,0 +1,59 @@
+package entity;
+
+import instancesOfDB.Client;
+import instancesOfDB.Order;
+import instancesOfDB.Product;
+
+import java.util.ArrayList;
+
+public class ListsFromDB {
+
+    private ArrayList<Client> clientList = new ArrayList<>();
+    private ArrayList<Product> productList = new ArrayList<>();
+    private ArrayList<Order> orderList = new ArrayList<>();
+
+    private static  ListsFromDB listsFromDB = new ListsFromDB();
+
+    public ListsFromDB() {
+    }
+
+    public static ListsFromDB getListsFromDB() {
+        return listsFromDB;
+    }
+
+    public ArrayList<Client> getClientList() {
+        return clientList;
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    public ArrayList<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void addClient (Client client){
+        clientList.add(client);
+    }
+
+    public void addProduct (Product product){
+        productList.add(product);
+    }
+
+    public void addOrder (Order order){
+        orderList.add(order);
+    }
+
+    public void clearClientList (){
+        clientList.clear();
+    }
+
+    public void clearProductList (){
+        productList.clear();
+    }
+
+    public void clearOrderList (){
+        orderList.clear();
+    }
+}
